c2863f65f81dfe66e20b51a01c7c65eff886dcc2
VentGrey <omarpurataf@gmail.com>
2019-02-19 00:25:59 -0600

    Added basic consts and structures

--- src/knn/mod.rs
+++ src/knn/mod.rs
@@ -1,3 +1,27 @@
+/* Consulted sources:
+ * https://www.neuraldesigner.com/learning/examples/iris_flowers_classification*/
+
+use rand::{Rng, SeedableRng, StdRng};
+//TODO: No hacer un cagadero en el namespace otra vez
+
+const PATH: &str = "iris.data";
+//El archivo iris deberá estar en la raiz del proyecto
+
+const ent_filas: f64 = 0.50;
+//Filas de entrenamiento
+
+//TODO: Implementar ARC + STDGPU (La ejecucion sobre GPU debería ayudar)
+//TODO: Investigar cómo carajos hacer lo de arriba ^
+
+
+pub struct iris_plant{
+    sepal_length: f64,
+    sepal_width: f64,
+    petal_length: f64,
+    petal_width: f64,
+}
+
+
 pub fn main() {
     
 }

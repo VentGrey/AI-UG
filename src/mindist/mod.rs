dab5bb1551464c3ecb8b84ef51266da28b688144
VentGrey <omarpurataf@gmail.com>
2019-02-20 12:29:40 -0600

    Added & finished functions

--- src/mindist/mod.rs
+++ src/mindist/mod.rs
@@ -1,22 +1,37 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::f64;
 
-struct data {
-    chara:[f64; 5],
-    lab: String,
+struct data{
+    carac:[f64; 5],
+    etiq: String,
 }
 
 pub fn main() -> std::io::Result<()> {
+
+    // Crear el archivo donde se entregarán los datos clasificados
     let mut file = File::create("Classsy.txt")?;
     Ok(())
     // La neta ya ni me acuerdo para que servía el Ok, pero si se lo quito
     // ya no jala :(
 }
 
-fn euclid() {
-    
+fn euclid(e:data, n:data) -> f64 {
+    let mut dist:f64 = 0.0;
+
+    for i in 0..= 5 {
+        dist += (e.carac[i] - n.carac[i]).powf(2.0);
+    }
+    dist = dist.sqrt();
+
+    // Return implícito
+    dist
 }
 
 fn manhattan() {
     
+}
+
+fn calc_cent() {
+
 }

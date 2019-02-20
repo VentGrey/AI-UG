1120d5805e52e007d5476ce47616a2bc0f41e60a
VentGrey <omarpurataf@gmail.com>
2019-02-20 07:35:08 -0600

    Added file management

--- src/mindist/mod.rs
+++ src/mindist/mod.rs
@@ -1,5 +1,16 @@
-pub fn main() {
-    
+use std::fs::File;
+use std::io::prelude::*;
+
+struct data {
+    chara:[f64; 5],
+    lab: String,
+}
+
+pub fn main() -> std::io::Result<()> {
+    let mut file = File::create("Classsy.txt")?;
+    Ok(())
+    // La neta ya ni me acuerdo para que servía el Ok, pero si se lo quito
+    // ya no jala :(
 }
 
 fn euclid() {

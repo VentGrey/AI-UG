20e9dd8af23fa39f4a359afbd8f70a3c116ad9a6
VentGrey <omarpurataf@gmail.com>
2019-02-22 22:31:00 -0600

    Added experimental config file comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,35 @@
 mod mindist; //Módulo de minima distancia
 mod knn; //Módulo "K-Nearest Neighbours" [EXPERIMENTAL]
 
+use colored::*;
+
 fn main() {
-    
+    println!("");
 }
+
+/* EXPERIMENTAL - ARCHIVOS DE CONFIGURACIÓN */
+
+// Utilizar configuraciónes es un desmadre, más que nada por que
+// los sistemas operativos son fans de ser contreras y nada jala
+// igual en todos, por lo que el código de abajo es un intento
+// de implementar un archivo de configuración en este programa.
+
+
+// NO aseguro que funcione, no por el momento.
+
+/*
+
+struct ConfigFile {
+    file: String,
+    output: String,
+    classifier: String,
+    charac: [i32],
+}
+
+fn load_conf() -> Result<(), io::Error> {
+    let config: ConfigFile = config::load("ai_ug")?;
+    dbg!(config);
+    Ok(())
+}
+
+*/

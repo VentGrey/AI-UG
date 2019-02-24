faa410c203366cdcaf90b211b1c21d9bbd5a48bb
VentGrey <omarpurataf@gmail.com>
2019-02-23 22:34:22 -0600

    Commented args part & added colored menu

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@
 mod mindist; //Módulo de minima distancia
 mod knn; //Módulo "K-Nearest Neighbours" [EXPERIMENTAL]
 
+// Colores ANSI en terminal, para que el usuario no la riegue
 use colored::*;
 
+
+// -- EXPERIMENTAL (Pase de parámetros por línea de comandos)
+// Parser de argumentos de línea de comandos
+// use std::env::args;
+
 fn main() {
-<<<<<<< HEAD
+    println!("{}","ai-ug BETA".yellow());
+    println!("{}","Seleccione el algoritmo que desea utilizar:\n
+              1- Clasificador de mínima distancia\n
+              2- K-Nearest-Neighbours\n
+              3- Uno que no me acuerdo como se llama\n
+              4- K-Means".green());
 
+
+
+    /* EXPERIMENTAL - Pase de parámetros
+    let progerror = "¡Programa desconocido!\nSaliendo...".red();
+    let filerror = "¡Error al abrir/crear archivo!".red();
+
+
+    let program = std::env::args().nth(1).expect(progerror);
+    let file = std::env::args().nth(2).expect(filerror);
+    */
 }
 
 /* EXPERIMENTAL - ARCHIVOS DE CONFIGURACIÓN */
@@ -28,30 +49,6 @@ struct ConfigFile {
     charac: [i32],
 }
 
-=======
-    println!("");
-}
-
-/* EXPERIMENTAL - ARCHIVOS DE CONFIGURACIÓN */
-
-// Utilizar configuraciónes es un desmadre, más que nada por que
-// los sistemas operativos son fans de ser contreras y nada jala
-// igual en todos, por lo que el código de abajo es un intento
-// de implementar un archivo de configuración en este programa.
-
-
-// NO aseguro que funcione, no por el momento.
-
-/*
-
-struct ConfigFile {
-    file: String,
-    output: String,
-    classifier: String,
-    charac: [i32],
-}
-
->>>>>>> 20e9dd8af23fa39f4a359afbd8f70a3c116ad9a6
 fn load_conf() -> Result<(), io::Error> {
     let config: ConfigFile = config::load("ai_ug")?;
     dbg!(config);

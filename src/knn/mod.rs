7e6cdb1c7ffcb38f7f7f797b1c3a98d21acc4088
VentGrey <omarpurataf@gmail.com>
2019-02-23 22:52:54 -0600

    Fixed reader input error

--- src/knn/mod.rs
+++ src/knn/mod.rs
@@ -1,11 +1,11 @@
 /* Consulted sources:
 * https://www.neuraldesigner.com/learning/examples/iris_flowers_classification*/
 
-use rand::prelude::*;
-
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 //El archivo iris deberá estar en la raiz del proyecto
 
-const ent_filas: f64 = 0.50;
+const rows_train: f64 = 0.50;
 //Filas de entrenamiento
 
 //TODO: Implementar ARC + STDGPU (La ejecucion sobre GPU debería ayudar)
@@ -18,18 +18,18 @@ pub struct IrisPlant {
     petal_width: f64,
 }
 
-pub fn main(PATH:&str) {
-    let mut iris_read = csv::Reader::from_reader(PATH).unwrap();
+pub fn main(PATH: &str) {
+    let mut iris_read = csv::Reader::from_reader(PATH.as_bytes());
     /* FIXME: La nueva versión de csv eliminó la función "from_reader"
     a ver como le hago */
 
-    let seed: &[_] = &[1, 2];
-
+    let seed: &[_] = &[1, 2, 3, 4];
     let mut rand: StdRng = SeedableRng::from_seed(seed);
+
     // FIXME: Error de tipos, ni idea, creo que mi arreglo está mal declarado
 
     for i in iris_read.decode() {
-        if rand.gen_range(0.0, 1.0) > ent_filas {
+        if rand.gen_range(0.0, 1.0) > rows_train {
             let iris_flower: IrisPlant = i.unwrap();
 
             println!("ls: {}", iris_flower.petal_length);
